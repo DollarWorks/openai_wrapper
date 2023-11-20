6a586c58362bf68c3a440f3c5c427ec3a8cca5ca
li_tian <heefan@msn.com>
2023-11-20 21:21:23 +0800

    close #7, deeplearningai - prompt engineering - principle2 - tactic 2 - instruct the model to work out its own solution before rushing to a conclusion
    [7]

--- tests/test_chat_completions_deeplearningai.py
+++ tests/test_chat_completions_deeplearningai.py
@@ -151,7 +151,7 @@ def test_p1_t4_few_shot():
 #   Principle 2: Give the model time to "think"
 ##################################################################
 # Tactic1: Specify the steps required to complete a task
-def test_p2_t1_steps():  # Principle2, Tactic1
+def test_p2_t1_steps_ex1():  # Principle2, Tactic1
     text = f"""
     In a charming village, siblings Jack and Jill set out on \
     a quest to fetch water from a hilltop \
@@ -198,3 +198,95 @@ def test_p2_t1_steps():  # Principle2, Tactic1
 
 
 
+
+# Tactic1-example2: Ask for output in a specific format
+def test_p2_t1_steps_ex2():  # Principle2, Tactic1
+    text = f"""
+    In a charming village, siblings Jack and Jill set out on \
+    a quest to fetch water from a hilltop \
+    well. As they climbed, singing joyfully, misfortune \
+    struck—Jack tripped on a stone and tumbled \
+    down the hill, with Jill following suit. \
+    Though slightly battered, the pair returned home to \
+    comforting embraces. Despite the mishap, \
+    their adventurous spirits remained undimmed, and they \
+    continued exploring with delight.
+    """
+
+    prompt = f"""
+    Your task is to perform the following actions: 
+    1 - Summarize the following text delimited by 
+      <> with 1 sentence.
+    2 - Translate the summary into French.
+    3 - List each name in the French summary.
+    4 - Output a json object that contains the 
+      following keys: french_summary, num_names.
+    
+    Use the following format:
+    Text: <text to summarize>
+    Summary: <summary>
+    Translation: <summary translation>
+    Names: <list of names in Italian summary>
+    Output JSON: <json with summary and num_names>
+    
+    Text: <{text}>
+    """
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful assistant and output in JSON"
+        }, 
+        {
+            "role": "user",
+            "content": prompt,
+        }]
+
+    try:
+        response = fetch_chat_completion_json(client, messages)
+        LOG_D(response.choices[0].message.content)
+    except Exception:
+        assert False, "An exception was raised"
+
+
+
+## Tactic 2: Instruct the model to work out its own solution before rushing to a conclusion
+def test_p2_t2_own_solution():  # Principle2, Tactic1
+
+    prompt = f"""
+    Determine if the student's solution is correct or not.
+
+    Question:
+    I'm building a solar power installation and I need \
+     help working out the financials. 
+    - Land costs $100 / square foot
+    - I can buy solar panels for $250 / square foot
+    - I negotiated a contract for maintenance that will cost \
+    me a flat $100k per year, and an additional $10 / square \
+    foot
+    What is the total cost for the first year of operations
+    as a function of the number of square feet.
+
+    Student's Solution:
+    Let x be the size of the installation in square feet.
+    Costs:
+    1. Land cost: 100x
+    2. Solar panel cost: 250x
+    3. Maintenance cost: 100,000 + 100x
+    Total cost: 100x + 250x + 100,000 + 100x = 450x + 100,000
+    """
+
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful assistant and output in JSON"
+        }, 
+        {
+            "role": "user",
+            "content": prompt,
+        }]
+
+    try:
+        response = fetch_chat_completion_json(client, messages)
+        LOG_D(response.choices[0].message.content)
+    except Exception:
+        assert False, "An exception was raised"
